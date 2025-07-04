f472f9cef1bc0af2b340a0dc5c2d02d04440e583
Your Name <you@example.com>
2025-07-05 01:09:57 +0300

    изменено:      python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -1,2 +1,12 @@
-curl -L -o python_3.9.7_arm.deb  https://github.com/Termux-pod/termux-pod/raw/refs/heads/main/aarch64/python/python-3.9.7/python_3.9.7_aarch64.deb
-dpkg -i ./python_3.9.7_arm.deb
+#curl -L -o python_3.9.7_arm.deb  https://github.com/Termux-pod/termux-pod/raw/refs/heads/main/aarch64/python/python-3.9.7/python_3.9.7_aarch64.deb
+#dpkg -i ./python_3.9.7_arm.deb
+
+
+wget https://www.python.org/ftp/python/3.9.23/Python-3.9.23.tgz
+tar xzf Python-3.9.23.tgz
+cd Python-3.9.23
+
+ ./configure --prefix=/opt/python/3.9.23/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --with-openssl=/usr/local/ssl
+ make -j "$(grep -c ^processor /proc/cpuinfo)"
+ make altinstall
+ rm /tmp/Python-3.9.23.tgz
