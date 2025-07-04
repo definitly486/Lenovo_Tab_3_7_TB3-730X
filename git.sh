7bad7a30332fb92c3cbfbb0c02a0157bdee7048c
Your Name <you@example.com>
2025-07-04 21:04:28 +0300

    изменено:      git.sh

--- git.sh
+++ git.sh
@@ -6,5 +6,5 @@ git config --global user.name "Your Name"
 git add --all
 git add .
 git commit -n
-echo definitly486 | git push https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X.git
+git push https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X.git
 
