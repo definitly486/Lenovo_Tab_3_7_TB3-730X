1aa9a762dce82f6e38526c99af1912b5466b599d
Your Name <you@example.com>
2025-07-05 00:45:33 +0300

    изменено:      python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -1,2 +1,2 @@
-curl -L -o python_3.9.7_arm.deb  https://github.com/Termux-pod/termux-pod/blob/main/arm/python/python-3.9.7/python_3.9.7_arm.deb
+curl -L -o python_3.9.7_arm.deb  https://github.com/Termux-pod/termux-pod/raw/refs/heads/main/arm/python/python-3.9.7/python_3.9.7_arm.deb
 dpkg -i ./python_3.9.7_arm.deb
