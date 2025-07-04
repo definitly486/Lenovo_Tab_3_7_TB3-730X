e7f1c97524a9c94ef4e075a534f2daa98d1b1d27
Your Name <you@example.com>
2025-07-04 15:29:11 +0300

    изменено:      install_apk.sh

--- install_apk.sh
+++ install_apk.sh
@@ -3,13 +3,14 @@ if ! [  -f "apk.tar.xz" ]; then
      curl -L -o apk.tar.xz     http://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/apk.tar.xz   
 fi
 
-if ! [  -f "apk" ]; then
+if ! [  -d "apk" ]; then
      tar xf apk.tar.xz 
 fi 
 
+cd apk
 
   for i in $(cat apk_list ); do
 
-             pm install  apk/$i 
+             pm install  $i 
 
 done
