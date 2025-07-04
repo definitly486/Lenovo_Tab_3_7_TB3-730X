3252a8fb9d121abe14a7438bd401975a8316ef5b
Your Name <you@example.com>
2025-07-04 15:13:02 +0300

    изменено:      install_apk.sh

--- install_apk.sh
+++ install_apk.sh
@@ -1,3 +1,2 @@
-curl -O https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/apk.tar.xz
-
+curl -L -o apk.tar.xz     http://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/apk.tar.xz   
 tar xf apk.tar.xz 
