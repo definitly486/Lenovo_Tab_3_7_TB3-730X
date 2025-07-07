30737e1e29907fb364991f278a4ee6b461fd2a96
Your Name <you@example.com>
2025-07-07 14:04:38 +0300

    изменено:      install_apk.sh

--- install_apk.sh
+++ install_apk.sh
@@ -1,5 +1,7 @@
 DIR=$(dirname "$(realpath $0)")
 
+cd $DIR/storage/downloads
+
 if ! [  -f "apk.tar.xz" ]; then
      curl -L -o apk.tar.xz     http://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/apk.tar.xz   
 fi
