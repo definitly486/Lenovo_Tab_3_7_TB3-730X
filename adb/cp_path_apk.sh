6ec3530bf4b2f9ebfd703aad3f8500bd3ce25fb1
Your Name <you@example.com>
2025-07-07 16:39:21 +0300

    изменено:      adb/cp_path_apk.sh

--- adb/cp_path_apk.sh
+++ adb/cp_path_apk.sh
@@ -2,5 +2,5 @@
 DIR=$(dirname "$(realpath $0)")
 
 for i in $(cat $DIR/apk_path ); do
-      su - root -c  "cp  -r $i  ../storage/downloads"
+      su - root -c  "cp  -R $i  ../storage/downloads"
 done
