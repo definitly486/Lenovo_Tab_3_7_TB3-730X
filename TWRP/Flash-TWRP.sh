b80621b6870594afb6dc76d8796ce792e6d30875
Your Name <you@example.com>
2025-07-07 17:41:59 +0300

    изменено:      TWRP/Flash-TWRP.sh 	изменено:      adb/apk_path 	удалено:       adb/cp_path_apk.sh

--- TWRP/Flash-TWRP.sh
+++ TWRP/Flash-TWRP.sh
@@ -2,3 +2,4 @@
 /usr/local/bin/adb reboot bootloader 
 /usr/local/bin/fastboot oem unlock
 /usr/local/bin/fastboot flash recovery twrp.img
+/usr/local/bin/fastboot  reboot recovery
--- adb/cp_path_apk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DIR=$(dirname "$(realpath $0)")
-
-for i in $(cat $DIR/apk_path ); do
-      su - root -c  "cp  -R $i  ../storage/downloads"
-done
