b9db58679b352fbb7aa04f06fad8481c416d1f81
Your Name <you@example.com>
2025-07-08 08:54:09 +0300

    изменено:      adb/apk_path 	новый файл:    install_proxy.sh 	удалено:       run_proxy.sh

---
+++ install_proxy.sh
@@ -0,0 +1,2 @@
+
+bash <(curl -fsSL https://raw.githubusercontent.com/larionit/opera-proxy-install/main/opera-proxy-install-android.sh)
--- run_proxy.sh
+++
@@ -1,2 +0,0 @@
-
-./opera-proxy.android-arm  -bind-address 127.0.0.1:18080
