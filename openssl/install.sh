86a08736aa82b76894d514a113f48090d9f11539
Your Name <you@example.com>
2025-07-10 02:59:06 +0300

    изменено:      openssl/install.sh

--- openssl/install.sh
+++ openssl/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -L -o  openssl-1.0.2o.tar.gz  https://www.openssl.org/source/openssl-1.0.2o.tar.gz
-tar xz openssl-1.0.2o.tar.gz
+tar -xvzf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o
 ./config shared --prefix=/data/data/com.termux/files/usr/
 make 
