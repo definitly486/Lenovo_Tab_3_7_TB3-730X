dbfaa190b0d0396ef7e4235388ae8636e6a06623
Your Name <you@example.com>
2025-07-10 04:09:41 +0300

    изменено:      openssl/install.sh

--- openssl/install.sh
+++ openssl/install.sh
@@ -4,6 +4,7 @@ curl -L -o  openssl-1.0.2o.tar.gz  https://www.openssl.org/source/openssl-1.0.2o
 tar -xvzf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o
 export CPPFLAGS+=" -D__ANDROID_API__=21"
+export LDFLAGS=" -llog"
 ./config shared --prefix=/data/data/com.termux/files/usr/
 make 
 make install 
