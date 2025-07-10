9d41625fbc72fed63339b55883079efbda5e53b3
Your Name <you@example.com>
2025-07-11 00:17:49 +0300

    новый файл:    python/pyenv_install.sh

---
+++ python/pyenv_install.sh
@@ -0,0 +1,6 @@
+pkg install git
+curl -fsSL https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
