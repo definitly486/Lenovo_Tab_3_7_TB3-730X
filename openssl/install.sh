bab4b5a073d90a1adcabdf92211c665b8373b77b
Your Name <you@example.com>
2025-07-10 13:30:24 +0300

    изменено:      openssl/install.sh

--- openssl/install.sh
+++ openssl/install.sh
@@ -4,7 +4,8 @@ curl -L -o  openssl-1.0.2o.tar.gz  https://www.openssl.org/source/openssl-1.0.2o
 tar -xvzf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o
 export LDFLAGS=" -llog"
-./config shared --prefix=/data/data/com.termux/files/ zlib-dynamic  no-ssl no-hw no-srp no-tests
+rm -rf "$TERMUX_PREFIX/lib"/libcrypto.* "$TERMUX_PREFIX/lib"/libssl.*
+./config shared --prefix=/data/data/com.termux/files/usr zlib-dynamic  no-ssl no-hw no-srp no-tests
 make depend
 make all
 make install 
