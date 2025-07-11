c64195249edd34882b8128567eb6aad8f3e33145
Your Name <you@example.com>
2025-07-11 20:40:54 +0300

    изменено:      python/build_python.sh 	изменено:      python/python_install.sh

--- python/python_install.sh
+++ python/python_install.sh
@@ -11,7 +11,7 @@ cd Python-3.10.0
 export LDFLAGS+=" -L$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib"
 export CPPFLAGS+=" -D__ANDROID_API__=21 -I$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
 export LIBCRYPT_LIBS="-lcrypt"
-./configure --prefix=/data/data/com.termux/files/usr  --enable-optimizations
+./configure  --host=aarch64-linux-android  --prefix=/data/data/com.termux/files/usr  --enable-optimizations
 make -j1
 make altinstall
 
