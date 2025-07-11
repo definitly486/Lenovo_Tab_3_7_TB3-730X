31bce543c3305b4464fbb5d116d6e4eb26c7bb2f
Your Name <you@example.com>
2025-07-11 06:58:01 +0300

    новый файл:    python/build_python.sh

---
+++ python/build_python.sh
@@ -0,0 +1,8 @@
+DIR=$('pwd')
+cd Python-3.9.23
+export LDFLAGS+=" -L$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib -host=aarch64"
+export CPPFLAGS+=" -D__ANDROID_API__=27 -I$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
+export LIBCRYPT_LIBS="-lcrypt"
+./configure --prefix=/data/data/com.termux/files/usr  --enable-optimizations
+make -j1
+make altinstall
