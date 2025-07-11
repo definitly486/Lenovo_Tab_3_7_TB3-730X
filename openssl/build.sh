73c59c045e959cdf27a4a247754673315d40b84f
Your Name <you@example.com>
2025-07-11 19:47:31 +0300

    новый файл:    openssl/build.sh 	изменено:      openssl/install.sh

--- openssl/build.sh
+++ openssl/build.sh
@@ -1,7 +1,3 @@
-#!/bin/sh
-
-curl -L -o  openssl-1.0.2o.tar.gz  https://www.openssl.org/source/openssl-1.0.2o.tar.gz
-tar -xvzf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o
 export LDFLAGS=" -llog"
 export LIBCRYPT_LIBS="-lcrypt"
