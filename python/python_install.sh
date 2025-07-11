149ca57d5df9db73cc73ac9c0351bd3aa09ce987
Your Name <you@example.com>
2025-07-11 06:16:39 +0300

    изменено:      python/python_install.sh

--- python/python_install.sh
+++ python/python_install.sh
@@ -1,12 +1,17 @@
 #curl -L -o python_3.9.7_arm.deb  https://github.com/Termux-pod/termux-pod/raw/refs/heads/main/aarch64/python/python-3.9.7/python_3.9.7_aarch64.deb
 #dpkg -i ./python_3.9.7_arm.deb
 
-
+DIR=$('pwd')
+pkg install git  build-essential
+curl -L -o android-ndk-r27c-linux.zip   https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
 curl -L -o Python-3.9.23.tgz  https://www.python.org/ftp/python/3.9.23/Python-3.9.23.tgz
+unzip android-ndk-r27c-linux.zip
 tar xzf Python-3.9.23.tgz
 cd Python-3.9.23
-export CPPFLAGS+=" -D__ANDROID_API__=21"
-./configure --prefix=/opt/python/3.9.23/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --with-openssl=/usr/local/ssl
-make -j "$(grep -c ^processor /proc/cpuinfo)"
+export LDFLAGS+=" -L$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib"
+export CPPFLAGS+=" -D__ANDROID_API__=21 -I$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
+export LIBCRYPT_LIBS="-lcrypt"
+./configure --prefix=/data/data/com.termux/files/usr  --enable-optimizations
+make -j1
 make altinstall
 rm /tmp/Python-3.9.23.tgz
