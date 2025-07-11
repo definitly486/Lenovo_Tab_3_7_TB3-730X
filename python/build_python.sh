e9d684d04195724e95e0c0b22ddec24a72d9238a
Your Name <you@example.com>
2025-07-11 20:47:51 +0300

    новый файл:    python/build_aarch64_python.sh 	изменено:      python/build_python.sh

--- python/build_python.sh
+++ python/build_python.sh
@@ -1,7 +1,6 @@
 DIR=$('pwd')
 cd Python-3.10.0
-export LDFLAGS+=" -L$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib"
-export CPPFLAGS+=" -D__ANDROID_API__=27 -I$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
+export CPPFLAGS+=" -D__ANDROID_API__=27"
 export LIBCRYPT_LIBS="-lcrypt"
 ./configure  --host=aarch64-linux-android --prefix=/data/data/com.termux/files/usr  --enable-optimizations
 make -j1
