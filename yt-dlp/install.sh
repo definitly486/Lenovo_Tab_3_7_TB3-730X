05ccca7e40e2bdee3212832c3b196be0f14889e4
Your Name <you@example.com>
2025-07-14 21:40:17 +0300

    новый файл:    yt-dlp/install.sh

---
+++ yt-dlp/install.sh
@@ -0,0 +1,4 @@
+curl -L -o yt_dlp_2024.10.22.tar.gz https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/yt-dlp/yt_dlp_2024.10.22.tar.gz
+tar xzf  yt_dlp_2024.10.22.tar.gz
+cp -R  yt_dlp_2024.10.22/yt-dlp  /data/data/com.termux/files/usr/lib/python3.9/site-packages
+cp  yt_dlp_2024.10.22/yt-dlp  /data/data/com.termux/files/usr/bin
