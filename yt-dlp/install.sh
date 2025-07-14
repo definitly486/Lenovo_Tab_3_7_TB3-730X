514e1fd64d90437d55abda83a4f3bd46f0697920
Your Name <you@example.com>
2025-07-14 22:41:38 +0300

    изменено:      yt-dlp/install.sh

--- yt-dlp/install.sh
+++ yt-dlp/install.sh
@@ -1,4 +1,4 @@
-pkg install make
+pkg install  build-essential
 curl -L -o  Python-3.9.23.build.tar.gz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python3.9/Python-3.9.23.build.tar.gz
 tar xzf  Python-3.9.23.build.tar.gz
 cd Python-3.9.23 
