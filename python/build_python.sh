60415534c52ab3046bf8eb8c203676839368a49d
Your Name <you@example.com>
2025-07-14 10:27:14 +0300

    изменено:      python/build_python.sh 	изменено:      python/python_install.sh

--- python/build_python.sh
+++ python/build_python.sh
@@ -1,7 +1,7 @@
 DIR=$('pwd')
 cd Python-3.10.0
-export CPPFLAGS+=" -D__ANDROID_API__=27"
+export CPPFLAGS+=" -D__ANDROID_API__=21"
 export LIBCRYPT_LIBS="-lcrypt"
-./configure   --prefix=/data/data/com.termux/files/usr  --enable-optimizations --with-openssl=$HOME/Lenovo_Tab_3_7_TB3-730X/openssl/openssl-1.0.2o
+./configure   --prefix=/data/data/com.termux/files/usr   --with-openssl=$HOME/Lenovo_Tab_3_7_TB3-730X/openssl/openssl-1.0.2o
 make -j1
 make altinstall
--- python/python_install.sh
+++ python/python_install.sh
@@ -3,15 +3,14 @@
 
 DIR=$('pwd')
 pkg install git  build-essential
-curl -L -o android-ndk-r27c-linux.zip   https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
+#curl -L -o android-ndk-r27c-linux.zip   https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
 curl -L -o Python-3.10.0.tgz  https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
-unzip android-ndk-r27c-linux.zip
+#unzip android-ndk-r27c-linux.zip
 tar xzf Python-3.10.0.tgz
 cd Python-3.10.0
-export LDFLAGS+=" -L$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib"
-export CPPFLAGS+=" -D__ANDROID_API__=21 -I$DIR/android-ndk-r27c/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
+export CPPFLAGS+=" -D__ANDROID_API__=21"
 export LIBCRYPT_LIBS="-lcrypt"
-./configure  --host=aarch64-linux-android  --prefix=/data/data/com.termux/files/usr  --enable-optimizations
+./configure   --prefix=/data/data/com.termux/files/usr
 make -j1
 make altinstall
 
