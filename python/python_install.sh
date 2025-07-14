f98f6895b3e507c33fad6735ab428df87a9980a3
Your Name <you@example.com>
2025-07-14 21:25:51 +0300

    изменено:      python/python_install.sh

--- python/python_install.sh
+++ python/python_install.sh
@@ -9,7 +9,7 @@ pkg install git  build-essential
 #make -j1
 #make altinstall
 
-curl -L -o  Python-3.9.23.build.tar.gz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/untagged-661a0102b529272aa208/Python-3.9.23.build.tar.gz
+curl -L -o  Python-3.9.23.build.tar.gz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python3.9/Python-3.9.23.build.tar.gz
 tar xzf  Python-3.9.23.build.tar.gz
 cd Python-3.9.23 
 make install 
