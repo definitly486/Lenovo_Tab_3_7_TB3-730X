7770b2217686452962d00ef7b851ec1b063fcbd8
Your Name <you@example.com>
2025-07-15 20:23:28 +0300

    изменено:      python/python3.10_install.sh

--- python/python3.10_install.sh
+++ python/python3.10_install.sh
@@ -1,7 +1,7 @@
 #DIR=$('pwd')
 pkg install git  build-essential
-curl -L -o Python-3.10.0.tgz  https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
-tar xzf Python-3.10.0.tgz
+curl -L -o  Python-3.10.0.build.tar.gz https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python/Python-3.10.0.build.tar.gz
+tar xzf Python-3.10.0.build.tar.gz
 cd Python-3.10.0
 make install 
 cp python /data/data/com.termux/files/usr/bin
