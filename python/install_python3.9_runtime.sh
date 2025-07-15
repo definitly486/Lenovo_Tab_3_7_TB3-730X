1d0edffa7e74dea14fd2c8e13efba254d14f2e04
Your Name <you@example.com>
2025-07-15 22:48:46 +0300

    новый файл:    python/install_python3.9_runtime.sh 	изменено:      streamlink/install.sh

---
+++ python/install_python3.9_runtime.sh
@@ -0,0 +1,11 @@
+curl -L -o  python3.9_runtime.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python3.9_runtime/python3.9_runtime.tar.xz
+tar xzf python3.9_runtime.tar.xz
+cp -R python3.9_runtime/python3.9  /data/data/com.termux/files/usr/lib/
+cp  python3.9_runtime/pip    /data/data/com.termux/files/usr/bin
+cp  python3.9_runtime/pip3    /data/data/com.termux/files/usr/bin
+cp  python3.9_runtime/pip3.9    /data/data/com.termux/files/usr/bin
+cp  python3.9_runtime/python    /data/data/com.termux/files/usr/bin
+cp  python3.9_runtime/python3    /data/data/com.termux/files/usr/bin
+cp  python3.9_runtime/python3.9_bak    /data/data/com.termux/files/usr/bin/python3.9
+cp  python3.9_runtime/python3.9-config    /data/data/com.termux/files/usr/bin/
+cp  python3.9_runtime/python3-config    /data/data/com.termux/files/usr/bin/
