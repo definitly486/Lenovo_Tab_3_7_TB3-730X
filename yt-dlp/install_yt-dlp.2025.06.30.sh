8e177a91ab5cfac4dd400acf88bc67af87df81b9
Your Name <you@example.com>
2025-07-16 18:57:04 +0300

    изменено:      yt-dlp/install_yt-dlp.2025.06.30.sh

--- yt-dlp/install_yt-dlp.2025.06.30.sh
+++ yt-dlp/install_yt-dlp.2025.06.30.sh
@@ -1,3 +1,2 @@
-curl -L -o yt-dlp https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/yt-dlp.2025.06.30/yt-dlp
-chmod +x yt-dlp 
-cp yt-dlp  /data/data/com.termux/files/usr/bin
+curl -L -o /data/data/com.termux/files/usr/bin/yt-dlp https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/yt-dlp.2025.06.30/yt-dlp
+chmod +x /data/data/com.termux/files/usr/bin/yt-dlp 
