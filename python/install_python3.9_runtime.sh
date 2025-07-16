cfba7fe38cb3eb93d3142b3c316c10b8f7a99429
Your Name <you@example.com>
2025-07-16 08:31:32 +0300

    изменено:      python/install_python3.9_runtime.sh

--- python/install_python3.9_runtime.sh
+++ python/install_python3.9_runtime.sh
@@ -13,3 +13,5 @@ cp  python3.9_runtime/python3    /data/data/com.termux/files/usr/bin
 cp  python3.9_runtime/python3.9_bak    /data/data/com.termux/files/usr/bin/python3.9
 cp  python3.9_runtime/python3.9-config    /data/data/com.termux/files/usr/bin/
 cp  python3.9_runtime/python3-config    /data/data/com.termux/files/usr/bin/
+chmod +x  /data/data/com.termux/files/usr/bin/python
+chmod +x  /data/data/com.termux/files/usr/bin/pip
