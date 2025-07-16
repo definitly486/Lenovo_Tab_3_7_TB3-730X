b47221d613827f00e89b965881c3bbe70be7be75
Your Name <you@example.com>
2025-07-16 08:42:57 +0300

    изменено:      streamlink/install.sh

--- streamlink/install.sh
+++ streamlink/install.sh
@@ -3,6 +3,6 @@ curl -L -o streamlink_cli.tar.xz                 https://github.com/definitly486
 tar xf streamlink.tar.xz
 tar xf streamlink_cli.tar.xz
 cp -R   streamlink/streamlink     /data/data/com.termux/files/usr/lib/python3.9/site-packages
-cp -R   streamlink/streamlink_cli /data/data/com.termux/files/usr/lib/python3.9/site-packages
+cp -R   streamlink_cli            /data/data/com.termux/files/usr/lib/python3.9/site-packages
 cp    streamlink/streamlink_bak /data/data/com.termux/files/usr/bin/streamlink
 chmod +x /data/data/com.termux/files/usr/bin/streamlink
