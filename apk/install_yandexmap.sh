2c1c0559aa564954f4cd25dcfaac31a507031fb1
Your Name <you@example.com>
2025-07-16 17:46:06 +0300

    изменено:      apk/install_yandexmap.sh

--- apk/install_yandexmap.sh
+++ apk/install_yandexmap.sh
@@ -1,4 +1,5 @@
+DIR=$(dirname "$(realpath $0)")
 if ! [  -f "Yandex_Maps_17.2.0.apk" ]; then
     curl -L -o maps.apk https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/Yandex_Maps_17.2.0.apk
 fi
-su - root -c  "pm install maps.apk"
+su - root -c  "pm install $DIR/maps.apk"
