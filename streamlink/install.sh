1b5790f9acc3aaaace9feee20f5decacd7e567be
Your Name <you@example.com>
2025-07-16 08:40:21 +0300

    изменено:      streamlink/install.sh

--- streamlink/install.sh
+++ streamlink/install.sh
@@ -1,5 +1,8 @@
-curl -L -o streamlink.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/streamlink.tar.xz
+curl -L -o streamlink.tar.xz                     https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/streamlink.tar.xz
+curl -L -o streamlink_cli.tar.xz                 https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/streamlink_cli.tar.xz
 tar xf streamlink.tar.xz
-cp -R   streamlink/streamlink /data/data/com.termux/files/usr/lib/python3.9/site-packages
+tar xf streamlink_cli.tar.xz
+cp -R   streamlink/streamlink     /data/data/com.termux/files/usr/lib/python3.9/site-packages
+cp -R   streamlink/streamlink_cli /data/data/com.termux/files/usr/lib/python3.9/site-packages
 cp    streamlink/streamlink_bak /data/data/com.termux/files/usr/bin/streamlink
 chmod +x /data/data/com.termux/files/usr/bin/streamlink
