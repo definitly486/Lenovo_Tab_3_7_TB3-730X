cdef131691422754e1bbbe55c245e81e2bb4a603
Your Name <you@example.com>
2025-07-22 01:48:21 +0300

    новый файл:    apk/install_googlemaps.sh

---
+++ apk/install_googlemaps.sh
@@ -0,0 +1,7 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=com.google.android.apps.maps_10.29.2-1029201242_minAPI21.apkmirror.com.apk
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
+fi
+su - root -c  "pm install /storage/emulated/0/Download/$FILE"
