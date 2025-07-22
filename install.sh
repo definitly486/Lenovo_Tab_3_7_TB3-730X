e4d024f932335cadc0ba3e33ed05c3f195c45c71
Your Name <you@example.com>
2025-07-23 00:33:18 +0300

    изменено:      install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR=$(dirname "$(realpath $0)")
 cp $DIR/yt-dlp/music_dl.sh /data/data/com.termux/files/usr/bin/music_dl
+cp $DIR/yt-dlp/video_dl.sh /data/data/com.termux/files/usr/bin/video_dl
 cp $DIR/slink    /data/data/com.termux/files/usr/bin/ 
