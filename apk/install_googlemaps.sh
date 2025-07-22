2a2e2fc92a771e7d93623d2278490a0a2de079cc
Your Name <you@example.com>
2025-07-22 11:22:17 +0300

    изменено:      apk/install_googlemaps.sh

--- apk/install_googlemaps.sh
+++ apk/install_googlemaps.sh
@@ -5,3 +5,10 @@ if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
     curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
 fi
 su - root -c  "pm install /storage/emulated/0/Download/$FILE"
+
+if ! [  -f "com.google.android.apps.maps.tar.xz" ]; then
+    curl -L -o  com.google.android.apps.maps.tar.xz     https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/googlemaps/com.google.android.apps.maps.tar.xz
+fi
+
+rm -R /data/data/com.google.android.apps.maps
+tar xzf com.google.android.apps.maps.tar.xz -C /data/data/
