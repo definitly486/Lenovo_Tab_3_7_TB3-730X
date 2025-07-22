b6a9c084ba7da21a792b403bdd34f716351bd82b
Your Name <you@example.com>
2025-07-22 18:36:50 +0300

    новый файл:    apk/install_libretorrent.sh

---
+++ apk/install_libretorrent.sh
@@ -0,0 +1,7 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=LibreTorrent-3.2-armeabi-v7a.apk 
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
+fi
+su - root -c  "pm install /storage/emulated/0/Download/$FILE"
