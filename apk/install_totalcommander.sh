1af191e841ceab1ec44dd1f6e4ce7901c3221d54
Your Name <you@example.com>
2025-07-22 21:43:36 +0300

    новый файл:    apk/install_totalcommander.sh

---
+++ apk/install_totalcommander.sh
@@ -0,0 +1,7 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=Total_Commander_v.3.60b4d.apk
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
+fi
+su - root -c  "pm install /storage/emulated/0/Download/apk/$FILE"
