a1a3c6f3387eb6d78449129c3546e599268a1790
Your Name <you@example.com>
2025-07-23 02:23:01 +0300

    изменено:      streamlink/install.sh

--- streamlink/install.sh
+++ streamlink/install.sh
@@ -7,7 +7,7 @@ curl -L -o wheel.tar.xz                          https://github.com/definitly486
 curl -L -o websocket.tar.xz                      https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/websocket.tar.xz 
 curl -L -o pycountry.tar.xz                      https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/pycountry.tar.xz
 curl -L -o charset_normalizer.tar.xz             https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/streamlink/charset_normalizer.tar.xz
-tar xf streamlink.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages              
+tar xf streamlink.tar.xz             
 tar xf streamlink_cli.tar.xz  -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages 
 tar xf exceptiongroup.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages  
 tar xf lxml.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages  
@@ -16,5 +16,6 @@ tar xf wheel.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-pac
 tar xf pycountry.tar.xz  -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages 
 tar xf websocket.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages  
 tar xf charset_normalizer.tar.xz -C    /data/data/com.termux/files/usr/lib/python3.9/site-packages 
-cp    /data/data/com.termux/files/usr/lib/python3.9/site-packages/streamlink/streamlink_bak /data/data/com.termux/files/usr/bin/streamlink
+cp    streamlink/streamlink_bak /data/data/com.termux/files/usr/bin/streamlink
+cp -R   streamlink/streamlink     /data/data/com.termux/files/usr/lib/python3.9/site-packages
 chmod +x /data/data/com.termux/files/usr/bin/streamlink
