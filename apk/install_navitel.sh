6a4dd867bccda697516ca95a27d284eb76c2d08f
Your Name <you@example.com>
2025-07-23 08:59:40 +0300

    изменено:      apk/install_navitel.sh

--- apk/install_navitel.sh
+++ apk/install_navitel.sh
@@ -7,3 +7,7 @@ fi
 cd  /data/data/com.termux/files/home/storage/downloads
 tar xf navitel.tar.xz
 su - root -c  "pm install /storage/emulated/0/Download/navitel/Navitel-9.13.46-full_crk_Q1-2020_armeabi-v7a.apk"
+mkdir -p  /storage/emulated/0/NavitelContent/Maps
+mkdir -p  /storage/emulated/0/NavitelContent/License
+mv   /storage/emulated/0/Download/navitel/pvfo20250416_v9.nm7 /storage/emulated/0/NavitelContent/Maps
+mv   /storage/emulated/0/Download/navitel/NaviTel_Activation_Key.txt /storage/emulated/0/NavitelContent/License
