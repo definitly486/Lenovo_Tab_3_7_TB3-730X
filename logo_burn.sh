862ad1fa817982d51a5ccdcb73006184047ca2f7
Your Name <you@example.com>
2025-07-23 04:59:04 +0300

    изменено:      logo_burn.sh

--- logo_burn.sh
+++ logo_burn.sh
@@ -6,5 +6,5 @@ fi
 su - root -c "dd if=$DIR/logo.bin_mod  of=/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/logo  bs=3145728 count=1"
 su - root -c "mount -o rw,remount /system"
 
-su - root -c "mv /system/bootanimation.zip /system/bootanimation1.zip"
-su - root -c "mv /system/shutanimation.zip /system/shutanimation1.zip"
+su - root -c "mv /system/media/bootanimation.zip /system/media/bootanimation1.zip"
+su - root -c "mv /system/media/shutanimation.zip /system/media/shutanimation1.zip"
