418bfa58397e5a26fc201d6d0034a6ae0de06581
Your Name <you@example.com>
2025-07-28 21:07:16 +0300

    новый файл:    apk/install_nekobox.sh 	изменено:      apk/install_pluma.sh

---
+++ apk/install_nekobox.sh
@@ -0,0 +1,7 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=NekoBox-1.3.8-arm64-v8a.apk 
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
+fi
+su - root -c  "pm install /storage/emulated/0/Download/$FILE"
