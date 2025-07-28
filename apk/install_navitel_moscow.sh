b883d86652de6644ae20a72e65b2c38277f2e016
Your Name <you@example.com>
2025-07-29 00:17:53 +0300

    изменено:      apk/install_navitel_moscow.sh

--- apk/install_navitel_moscow.sh
+++ apk/install_navitel_moscow.sh
@@ -6,6 +6,5 @@ if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
 fi
 cd  /data/data/com.termux/files/home/storage/downloads
 tar xf cfo20250225_v9.nm7.tar.xz 
-su - root -c  "pm install /storage/emulated/0/Download/navitel/Navitel-9.13.46-full_crk_Q1-2020_armeabi-v7a.apk"
 mkdir -p  /storage/emulated/0/NavitelContent/Maps
-mv   /storage/emulated/0/Download/navitel/cfo20250225_v9.nm7 /storage/emulated/0/NavitelContent/Maps
+mv   /storage/emulated/0/Download/cfo20250225_v9.nm7 /storage/emulated/0/NavitelContent/Maps
