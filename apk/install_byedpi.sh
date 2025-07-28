fd892fc8b3601e3d46143d8eaf6e10d434759473
Your Name <you@example.com>
2025-07-28 16:14:20 +0300

    изменено:      apk/install_byedpi.sh

--- apk/install_byedpi.sh
+++ apk/install_byedpi.sh
@@ -9,4 +9,5 @@ su - root -c  "pm install /storage/emulated/0/Download/$FILE"
 curl -L -o   io.github.dovecoteescapee.byedpi.tar.xz     https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/io.github.dovecoteescapee.byedpi.tar.xz
 tar xf io.github.dovecoteescapee.byedpi.tar.xz
 su - root -c "rm -r /data/data/io.github.dovecoteescapee.byedpi/"
-su - root -c "cp -r  $DIR/io.github.dovecoteescapee.byedpi   /data/data/" 
+su - root -c "cp -r  $DIR/io.github.dovecoteescapee.byedpi   /data/data/"
+su - root -c "chmod -R  775 /data/data/io.github.dovecoteescapee.byedpi/"
