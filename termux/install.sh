2de3f4ebf39bd005fdaccbc4bbbfbaf222637fd3
Your Name <you@example.com>
2025-07-30 02:21:06 +0300

    изменено:      termux/install.sh 	новый файл:    termux/install_ssh.sh 	изменено:      termux/sources.list

--- termux/install.sh
+++ termux/install.sh
@@ -1,4 +1,1 @@
-pkg install jq gnupg python
-cd $HOME
-git clone https://github.com/termux/termux-packages
-cp termux-setup-package-manager /data/data/com.termux/files/usr/bin
+cp sources.list /data/data/com.termux/files/usr/etc/apt
---
+++ termux/install_ssh.sh
@@ -0,0 +1,14 @@
+if [ -z "$1" ]
+then
+     echo "не введен пароль для распаковки ssh "
+     exit
+fi
+
+if ! [  -f "ssh.tar.xz.enc" ]; then
+     curl -L -o ssh.tar.xz.enc      https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/ssh.tar.xz.enc 
+fi
+
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in  ssh.tar.xz.enc  -out ssh.tar.xz       -pass pass:$1   
+tar xf ssh.tar.xz
+mkdir -p $HOME/.ssh
+cp ssh/* .ssh 
