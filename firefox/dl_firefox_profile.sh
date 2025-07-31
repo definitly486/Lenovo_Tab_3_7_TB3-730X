8252f4480e4a94fcc10d30869e57537c2c36770e
Your Name <you@example.com>
2025-07-31 14:09:51 +0300

    новый файл:    firefox/dl_firefox_profile.sh

---
+++ firefox/dl_firefox_profile.sh
@@ -0,0 +1,9 @@
+DIR=$(dirname "$(realpath $0)")
+if ! [  -f "org.mozilla.firefox.tar.xz" ]; then
+     curl -L -o   org.mozilla.firefox.tar.xz   https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/org.mozilla.firefox.tar.xz 
+fi
+tar xf org.mozilla.firefox.tar.xz
+
+su - root -c "rm -r /data/data/org.mozilla.firefox/files/mozilla"
+su - root -c "cp -r  $DIR/org.mozilla.firefox/files/mozilla /data/data/org.mozilla.firefox/files"
+su - root -c "chmod -R  755  /data/data/org.mozilla.firefox/files/mozilla"
