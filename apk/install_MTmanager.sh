68584b89c6bbf4e272107d0f8c893fc876b03046
Your Name <you@example.com>
2025-08-01 22:28:57 +0300

    новый файл:    apk/install_MTmanager.sh

---
+++ apk/install_MTmanager.sh
@@ -0,0 +1,9 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=MT2.17.2.apk 
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE     $URL/$FILE
+fi
+
+     su - root -c  "pm install /storage/emulated/0/Download/$FILE"
+
