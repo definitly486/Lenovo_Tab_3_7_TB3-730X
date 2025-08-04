3c73014682cdf595f842fb3f46727dec7bc876a8
Your Name <you@example.com>
2025-08-05 01:55:03 +0300

    изменено:      firefox/dl_telegram_profile.sh

--- firefox/dl_telegram_profile.sh
+++ firefox/dl_telegram_profile.sh
@@ -12,7 +12,7 @@ fi
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in  td.binlog.tar.xz.enc  -out  td.binlog.tar.xz     -pass pass:$1   
 tar xf td.binlog.tar.xz 
 ID=$(su - root -c "ls  -l /data/data/ | grep chall | head -n 1 | sed 's/[^ ]* //' |  cut -d' ' -f1" )
-su - root -c "chown $ID td.binlog"
+su - root -c "chown $ID:$ID td.binlog"
 su - root -c "rm  /data/data/org.thunderdog.challegram/files/tdlib/td.binlog"
 su - $ID  -c "cp  td.binlog /data/data/org.thunderdog.challegram/files/tdlib"
 
