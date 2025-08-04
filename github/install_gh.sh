08a86ff0bab16dc7682e3a5e24baa33e7bb783de
Your Name <you@example.com>
2025-08-04 19:14:20 +0300

    изменено:      github/install_gh.sh

--- github/install_gh.sh
+++ github/install_gh.sh
@@ -7,11 +7,11 @@ then
 fi
 
 DIR=$(dirname "$(realpath $0)")
-if ! [  -f "gh_2.76.2_linux_arm64.tar.gz" ]; then
-     curl -L -o   gh_2.76.2_linux_arm64.tar.gz   https://github.com/cli/cli/releases/download/v2.76.2/gh_2.76.2_linux_arm64.tar.gz
+if ! [  -f "gh_2.76.2_aarch64.tar.xz" ]; then
+     curl -L -o   gh_2.76.2_aarch64.tar.xz   https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/gh_2.76.2_aarch64/gh_2.76.2_aarch64.tar.xz
 fi
-tar xf gh_2.76.2_linux_arm64.tar.gz
-cp   $DIR/gh_2.76.2_linux_arm64/bin/gh   /data/data/com.termux/files/usr/bin/
+tar xf gh_2.76.2_aarch64.tar.xz
+cp   $DIR/gh_2.76.2_aarch64/gh   /data/data/com.termux/files/usr/bin/
 
 
 if ! [  -f "gh.tar.xz.enc" ]; then
