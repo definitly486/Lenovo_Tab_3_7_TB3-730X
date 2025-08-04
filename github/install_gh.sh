dae9092898015fbbcb76c48c306763f33f22aa5c
Your Name <you@example.com>
2025-08-04 18:21:28 +0300

    изменено:      github/install_gh.sh

--- github/install_gh.sh
+++ github/install_gh.sh
@@ -4,5 +4,4 @@ if ! [  -f "gh_2.76.2_linux_arm64.tar.gz" ]; then
      curl -L -o   gh_2.76.2_linux_arm64.tar.gz   https://github.com/cli/cli/releases/download/v2.76.2/gh_2.76.2_linux_arm64.tar.gz
 fi
 tar xf gh_2.76.2_linux_arm64.tar.gz
-su - root -c "cp   $DIR/gh_2.76.2_linux_arm64/bin/gh   /data/data/com.termux/files/usr/bin/"
-su - root -c "chmod +x  /data/data/com.termux/files/usr/bin/gh"
+cp   $DIR/gh_2.76.2_linux_arm64/bin/gh   /data/data/com.termux/files/usr/bin/
