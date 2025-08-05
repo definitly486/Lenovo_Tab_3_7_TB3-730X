236a9170f3db8baad33870a2c69ff32f6fa23b7a
Your Name <you@example.com>
2025-08-05 20:55:15 +0300

    изменено:      install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ cp $DIR/slink    /data/data/com.termux/files/usr/bin/
 
 curl -L -o /data/data/com.termux/files/usr/bin/yt-dlp https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/yt-dlp.2025.06.30/yt-dlp
 chmod +x /data/data/com.termux/files/usr/bin/yt-dlp 
+
+sh streamlink/install.sh
