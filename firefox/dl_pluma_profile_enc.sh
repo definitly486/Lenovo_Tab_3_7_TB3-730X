5e5403dfa6da855ccb78807de9394143a7b3a0b1
Your Name <you@example.com>
2025-08-05 19:20:29 +0300

    изменено:      firefox/dl_pluma_profile_enc.sh

--- firefox/dl_pluma_profile_enc.sh
+++ firefox/dl_pluma_profile_enc.sh
@@ -5,7 +5,7 @@ then
      exit
 fi
 DIR=$(dirname "$(realpath $0)")
-target_dir="${PATH}"
+target_dir=$(pkg list-installed | grep openssl-tool)
 
 dependency_packages=(
   "openssl-tool"
@@ -17,7 +17,7 @@ dependency_packages=(
 # Installing the required packages
 for package in "${dependency_packages[@]}"
 do
-    if [[ ! -f "$target_dir/$package" ]]; then
+    if [[ ! -f "$target_dir" ]]; then
         echo "Package '$package' was not found. Installation..."
         pkg install -y "$package" &> /dev/null 
             if [[ $? -eq 0 ]]; then
