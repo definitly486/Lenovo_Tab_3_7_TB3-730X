3390787e3c0f37929935b478d1ffb0a21fa05dbf
Your Name <you@example.com>
2025-08-05 19:29:22 +0300

    изменено:      firefox/dl_pluma_profile_enc.sh

--- firefox/dl_pluma_profile_enc.sh
+++ firefox/dl_pluma_profile_enc.sh
@@ -13,6 +13,19 @@ dependency_packages=(
 )
 
 
+unpack () {
+
+if ! [  -f "com.qflair.browserq.tar.xz.enc" ]; then
+     curl -L -o    com.qflair.browserq.tar.xz.enc    https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc 
+fi
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in   com.qflair.browserq.tar.xz.enc -out  com.qflair.browserq.tar.xz     -pass pass:$1   
+tar xf com.qflair.browserq.tar.xz
+su - root -c "mkdir -p /data/data/com.qflair.browserq"
+su - root -c "cp -r  $DIR/com.qflair.browserq/databases   /data/data/com.qflair.browserq"
+su - root -c "chmod -R  700  /data/data/com.qflair.browserq/databases"
+su - root -c "cp -r  $DIR/com.qflair.browserq/shared_prefs   /data/data/com.qflair.browserq"
+su - root -c "chmod -R  700  /data/data/com.qflair.browserq/shared_prefs"
+}
 
 # Installing the required packages
 for package in "${dependency_packages[@]}"
@@ -27,6 +40,8 @@ do
             fi
     else
         echo "Package '$package' is already installed."
+ unpack 
+
     fi
 done
 
