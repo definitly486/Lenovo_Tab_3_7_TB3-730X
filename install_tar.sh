9cbd10a2fd86bd3c18bc59ff9b57f6ce5d2296a0
Your Name <you@example.com>
2025-08-07 21:33:22 +0300

    изменено:      install_tar.sh

--- install_tar.sh
+++ install_tar.sh
@@ -4,3 +4,7 @@ su - root -c "cp /system/bin/tar /system/bin/tar.bak"
 su - root -c "echo '#!/system/bin/sh
                     busybox tar "$@"' > /system/bin/tar"
 su - root -c chmod 0755 /system/bin/tar 
+
+su - root -c "echo '#!/system/bin/sh
+                    busybox tar "$@"' > /system/bin/vi"
+su - root -c chmod 0755 /system/bin/vi
