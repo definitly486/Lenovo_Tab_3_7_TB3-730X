39059c87520ddedb9003c5a166c353201ae1a6ff
Your Name <you@example.com>
2025-08-08 01:28:01 +0300

    изменено:      python/install_python3.9_runtime.sh 	удалено:       python/pip_install.sh

--- python/install_python3.9_runtime.sh
+++ python/install_python3.9_runtime.sh
@@ -1,18 +1,50 @@
 
+
+su_install_python() {
+
+tar xf python3.9_runtime.tar.xz
+su - root -c cp -R python3.9_runtime/python3.9          $PATH_TERMUX/lib/
+su - root -c cp  python3.9_runtime/pip                  $PATH_TERMUX/bin
+su - root -c cp  python3.9_runtime/pip3                 $PATH_TERMUX/bin
+su - root -c cp  python3.9_runtime/pip3.9               $PATH_TERMUX/bin
+su - root -c cp  python3.9_runtime/python               $PATH_TERMUX/bin
+su - root -c cp  python3.9_runtime/python3              $PATH_TERMUX/bin
+su - root -c cp  python3.9_runtime/python3.9_bak        $PATH_TERMUX/bin/python3.9
+su - root -c cp  python3.9_runtime/python3.9-config     $PATH_TERMUX/bin/
+su - root -c cp  python3.9_runtime/python3-config       $PATH_TERMUX/bin/
+su - root -c chmod +x  $PATH_TERMUX/bin/python
+su - root -c chmod +x  $PATH_TERMUX/bin/python3
+su - root -c chmod +x  $PATH_TERMUX/bin/pip
+
+}
+
+
+if [  -f "export | grep TERMUX__HOME" ]; then
+    echo "переменная TERMUX__HOME существует "
+    PATH_TERMUX=/data/data/com.termux/files/usr/
+else
+    su - root -c "mount -o rw,remount /system"
+    PATH_TERMUX=/system
+    su_install_python
+fi
+
+
+
+
 if ! [  -f "python3.9_runtime.tar.xz" ]; then
-     curl -L -o  python3.9_runtime.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python3.9_runtime/python3.9_runtime.tar.xz
+     curl -k  -L -o  python3.9_runtime.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/python3.9_runtime/python3.9_runtime.tar.xz
 fi
 
 tar xf python3.9_runtime.tar.xz
-cp -R python3.9_runtime/python3.9  /data/data/com.termux/files/usr/lib/
-cp  python3.9_runtime/pip    /data/data/com.termux/files/usr/bin
-cp  python3.9_runtime/pip3    /data/data/com.termux/files/usr/bin
-cp  python3.9_runtime/pip3.9    /data/data/com.termux/files/usr/bin
-cp  python3.9_runtime/python    /data/data/com.termux/files/usr/bin
-cp  python3.9_runtime/python3    /data/data/com.termux/files/usr/bin
-cp  python3.9_runtime/python3.9_bak    /data/data/com.termux/files/usr/bin/python3.9
-cp  python3.9_runtime/python3.9-config    /data/data/com.termux/files/usr/bin/
-cp  python3.9_runtime/python3-config    /data/data/com.termux/files/usr/bin/
-chmod +x  /data/data/com.termux/files/usr/bin/python
-chmod +x  /data/data/com.termux/files/usr/bin/python3
-chmod +x  /data/data/com.termux/files/usr/bin/pip
+cp -R python3.9_runtime/python3.9          $PATH_TERMUX/lib/
+cp  python3.9_runtime/pip                  $PATH_TERMUX/bin
+cp  python3.9_runtime/pip3                 $PATH_TERMUX/bin
+cp  python3.9_runtime/pip3.9               $PATH_TERMUX/bin
+cp  python3.9_runtime/python               $PATH_TERMUX/bin
+cp  python3.9_runtime/python3              $PATH_TERMUX/bin
+cp  python3.9_runtime/python3.9_bak        $PATH_TERMUX/bin/python3.9
+cp  python3.9_runtime/python3.9-config     $PATH_TERMUX/bin/
+cp  python3.9_runtime/python3-config       $PATH_TERMUX/bin/
+chmod +x  $PATH_TERMUX/bin/python
+chmod +x  $PATH_TERMUX/bin/python3
+chmod +x  $PATH_TERMUX/bin/pip
