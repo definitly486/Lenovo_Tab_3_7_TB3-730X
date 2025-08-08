baa0f2df166e990cfea77824f7ab7181c412ba26
Your Name <you@example.com>
2025-08-08 17:31:41 +0300

    удалено:       install_tar.sh 	новый файл:    termux/install_tar.sh 	новый файл:    termux/killall 	новый файл:    termux/tar 	новый файл:    termux/vi 	новый файл:    termux/wget

---
+++ termux/install_tar.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+su - root -c mount -o rw,remount /system
+su - root -c cp /system/bin/tar /system/bin/tar.bak
+
+su - root -c cp tar   /system/bin/
+su - root -c cp  vi      /system/bin/
+su - root -c cp  wget    /system/bin/
+su - root -c cp  killall      /system/bin/
+su - root -c chmod 0755 /system/bin/tar 
+su - root -c chmod 0755 /system/bin/vi
+su - root -c chmod 0755 /system/bin/wget
+su - root -c chmod 0755 /system/bin/killall
+
+su - root -c curl -k -L -o /system/bin/jq  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/curl_openssl/jq
+su - root -c chmod 0755 /system/bin/jq
