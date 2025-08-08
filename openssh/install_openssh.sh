ab869b7f46d2b8d299d9c862a33215deb24b8fd5
Your Name <you@example.com>
2025-08-08 13:41:49 +0300

    изменено:      openssh/install_openssh.sh

--- openssh/install_openssh.sh
+++ openssh/install_openssh.sh
@@ -1,3 +1,6 @@
+
+PATH_ROOT=/system
+
 if ! [  -f "openssh_libs.tar.xz" ]; then
      curl -k  -L -o  openssh_libs.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/curl_openssl/openssh_libs.tar.xz
 fi
@@ -8,9 +11,9 @@ fi
 
 
 su - root -c cp      openssh_libs/*              $PATH_TERMUX/lib64/
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libedit.so"
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libcrypto.so"
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libncurses.so"
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libssl.so"
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libldns.so"
-su - root -c "chmod 0755  $PATH_TERMUX/lib64/libselinux.so.1"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libedit.so"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libcrypto.so"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libncurses.so"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libssl.so"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libldns.so"
+su - root -c "chmod 0755  $PATH_ROOT/lib64/libselinux.so.1"
