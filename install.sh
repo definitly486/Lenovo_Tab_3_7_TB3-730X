f1ebd4c725579fa274a86b46678cf9bbdc382a0b
Your Name <you@example.com>
2025-08-08 05:48:19 +0300

    изменено:      install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ cp $DIR/yt-dlp/music_dl.sh /data/data/com.termux/files/usr/bin/music_dl
 cp $DIR/yt-dlp/video_dl.sh /data/data/com.termux/files/usr/bin/video_dl
 cp $DIR/slink    /data/data/com.termux/files/usr/bin/ 
 
+
 curl -k -L -o /data/data/com.termux/files/usr/bin/yt-dlp https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/yt-dlp.2025.06.30/yt-dlp
 chmod +x /data/data/com.termux/files/usr/bin/yt-dlp 
 
 su - root -c "mount -o rw,remount /system"
 su - root -c "curl -k -L -o /system/bin/yt-dlp https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/curl_openssl/yt-dlp"
 su - root -c "chmod 0755 /system/bin/yt-dlp"
+su - root -c "cp $DIR/yt-dlp/music_dl.sh /system/bin/music_dl"
+su - root -c "cp $DIR/yt-dlp/video_dl.sh /system/bin/video_dl"
+su - root -c "cp $DIR/slink    /system/bin/"
 
 sh streamlink/install.sh
-
 cp termux/getbin.sh    /data/data/com.termux/files/usr/bin/getbin
 su - root -c "cp termux/getbin.sh    /system/bin/getbin"
