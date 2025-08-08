0f5357f0617685e29ff94af45aca5886fff158da
Your Name <you@example.com>
2025-08-08 14:11:44 +0300

    изменено:      openssh/install_openssh.sh 	новый файл:    openssh/openssh_bin_wrapper

--- openssh/install_openssh.sh
+++ openssh/install_openssh.sh
@@ -2,7 +2,7 @@
 PATH_ROOT=/system
 
 if ! [  -f "openssh_libs.tar.xz" ]; then
-     curl -k  -L -o  openssh_libs.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/curl_openssl/openssh_libs.tar.xz
+     curl -k  -L -o  openssh_libs.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/openssh/openssh_libs.tar.xz
 fi
 
 if ! [  -d openssh_libs"" ]; then
@@ -10,10 +10,17 @@ if ! [  -d openssh_libs"" ]; then
 fi 
 
 
-su - root -c cp      openssh_libs/*     $PATH_ROOT/lib64/
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libedit.so"
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libcrypto.so"
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libncurses.so"
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libssl.so"
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libldns.so"
-su - root -c "chmod 0755  $PATH_ROOT/lib64/libselinux.so.1"
+if ! [  -f "openssh_bin.tar.xz" ]; then
+     curl -k  -L -o  openssh_bin.tar.xz  https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/openssh/openssh_bin.tar.xz
+fi
+
+if ! [  -d openssh_bin"" ]; then
+      tar xf openssh_bin.tar.xz
+fi 
+
+
+su - root -c "mount -o rw,remount /system"
+su - root -c cp   -R    openssh_bin    $PATH_ROOT/usr/
+su - root -c "chmod  -R 0755  $PATH_ROOT/usr/openssh_bin"
+su - root -c cp   -R    openssh_libs    $PATH_ROOT/lib64/
+su - root -c "chmod  -R 0755  $PATH_ROOT/lib64/openssh_libs"
