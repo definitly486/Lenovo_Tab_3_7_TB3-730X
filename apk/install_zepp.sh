f5afa2f92f8f106348a5c9cfc76b07b127fcd032
Your Name <you@example.com>
2025-08-29 12:07:17 +0300

    новый файл:    apk/install_zepp.sh

---
+++ apk/install_zepp.sh
@@ -0,0 +1,11 @@
+DIR=$(dirname "$(realpath $0)")
+URL=https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/
+FILE=Zepp_8.5.1-play.151213.apk
+if ! [  -f "/data/data/com.termux/files/home/storage/downloads/$FILE.tar.xz" ]; then
+    curl -L -o /data/data/com.termux/files/home/storage/downloads/$FILE.tzr.xz     $URL/$FILE.tar.xz
+fi
+
+tar xf $FILE.tar.gz
+
+     su - root -c  "pm install /storage/emulated/0/Download/$FILE"
+
